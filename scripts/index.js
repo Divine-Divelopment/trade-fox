bae2b6d0a5ea3c931572b1a5f9934989d04db250
Ivan Baidiuk <ivan.baidiuk@ning.com>
2021-09-30 21:59:13 +0300

    slider

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+	effect: "coverflow",
+	grabCursor: false,
+	loop: false,
+	centeredSlides: true,
+	slidesPerView: "auto",
+	initialSlide: 1,
+	dragable: false,
+	noSwiping: false,
+	mousewheel: false,
+	coverflowEffect: {
+		rotate: 0,
+		stretch: 0,
+		depth: 50,
+		modifier: 1,
+		slideShadows: false
+	},
+	pagination: {
+		el: ".swiper-pagination"
+	}
+});
