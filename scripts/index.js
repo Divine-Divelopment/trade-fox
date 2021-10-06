ba9f73ec84a7a4470557fde2a5a8bedca122c15d
Ivan Baidiuk <ivan.baidiuk@ning.com>
2021-10-06 23:26:42 +0300

    lang-selector

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,52 @@
-var cowerflow = new Swiper(".mySwiper", {
-	effect: "coverflow",
+
+const select = document.querySelector('.language-select-list');
+const baseUrl = "./assets";
+
+function formatState (state) {
+  if (!state.id) {
+    return state.text;
+  }
+  
+  const currentState = document.createElement('span');
+  currentState.classList.add('selected-element')
+  currentState.innerHTML = '<img width="20" height="20" class="selection-flag" /> <span class="selection-text"></span>';
+
+  // Use .text() instead of HTML string concatenation to avoid script injection issues
+  currentState.querySelector(".selection-text").textContent = state.text;
+  currentState.querySelector(".selection-flag").setAttribute("src", baseUrl + "/" + state.element.value + ".svg");
+
+  return currentState;
+};
+
+function formatCountry (country) {
+	if (!country.id) {
+	 return country.text; 
+	}
+	  
+  const currentState = document.createElement('span');
+ 	changedCountry = document.createElement('span')
+	changedCountry.classList.add('selected-element')
+	changedCountry.innerHTML = '<img width="20" height="20" class="selection-flag" /> <span class="selection-text"></span>';
+
+	changedCountry.querySelector(".selection-text").textContent = country.text;
+  changedCountry.querySelector(".selection-flag").setAttribute("src", baseUrl + "/" + country.element.value + ".svg");
+	return changedCountry;
+};
+const select2 = new TsSelect2(
+	select, {
+	  templateSelection: formatState,
+	  templateResult:  formatCountry,
+	  minimumResultsForSearch: Infinity,
+	  width: `87px`
+	}
+);
+
+var cowerflow = new Swiper('.mySwiper', {
+	effect: 'coverflow',
 	grabCursor: false,
 	loop: false,
 	centeredSlides: true,
-	slidesPerView: "auto",
+	slidesPerView: 'auto',
 	initialSlide: 1,
 	dragable: false,
 	noSwiping: false,
@@ -16,28 +59,30 @@ var cowerflow = new Swiper(".mySwiper", {
 		slideShadows: false
 	},	
 	pagination: {
-		el: ".swiper-pagination"
+		el: '.swiper-pagination'
 	}
 });
 
 const swiper = new Swiper('.our-team-slider', {
   slidesPerView: 1,
-  spaceBetween: 10,
+  spaceBetween: 0,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
   breakpoints: {
     // when window width is >= 320px
     320: {
       slidesPerView: 2,
-      spaceBetween: 20
     },
     // when window width is >= 480px
     480: {
       slidesPerView: 3,
-      spaceBetween: 30
     },
     // when window width is >= 640px
     1024: {
       slidesPerView: 4,
-      spaceBetween: 40
     }
   }
 })
